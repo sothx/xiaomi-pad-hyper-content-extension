21e516abfd268c77535dc7235dc2deb2a2b7968f
sothx <i@sothx.com>
2024-05-13 11:43:27 +0800

    feat: 优化模块思路

--- README.md
+++ README.md
@@ -2,15 +2,19 @@
 
 ## 模块简介
 
-本Magisk模块用于修复Hyper OS For Pad 有关 [传送门] 相关的系统应用权限。
+本Magisk模块用于修复Hyper OS For Pad 有关 [传送门] 相关的系统应用权限，并为小米平板补全传送门。
 
 ## 模块该怎么使用？
 
-本Magisk模块需自行安装[传送门]相关Apk，并且使用[Hyper Ceiler]开启传送门相关配置，传送门系统UI界面位于[设置-更多设置-传送门]内。
+本Magisk模块需使用[Hyper Ceiler]或者[创建快捷方式]开启传送门相关配置，传送门系统UI界面位于[设置-更多设置-传送门]内。
 
 传送门Apk下载:
 
 链接:  https://caiyun.139.com/m/i?135ClTgdI5mRV
+
+创建快捷方式:
+
+链接:  https://caiyun.139.com/m/i?135Cm7bBJVOQv
 
 HyperCeiler下载:
 
--- customize.sh
+++ customize.sh
@@ -1,6 +1,10 @@
 # shellcheck disable=SC2148
 # shellcheck disable=SC2034
 SKIPUNZIP=0
+
+NODULE_MIUIContentExtension_APK_PATH=$MODPATH/system/product/priv-app/MIUIContentExtension/MIUIContentExtension.apk
+SYSTEM_NODULE_MIUIContentExtension_APK_PATH=/system/product/priv-app/MIUIContentExtension/MIUIContentExtension.apk
+
 if [[ "$KSU" == "true" ]]; then
   ui_print "- KernelSU 用户空间当前的版本号: $KSU_VER_CODE"
   ui_print "- KernelSU 内核空间当前的版本号: $KSU_KERNEL_VER_CODE"
@@ -12,4 +16,18 @@ else
     abort "*********************************************"
   fi
 fi
-ui_print "- 好诶，《HyperOS For Pad 传送门补丁》安装/更新完成，重启系统后生效！"
+
+
+
+ui_print "- 正在为你安装传送门，请稍等~"
+
+pm install -r -f  $NODULE_MIUIContentExtension_APK_PATH
+HAS_BEEN_INSTALLED_MIUIContentExtension_APK=$(pm list packages | grep com.miui.contentextension)
+sleep 6
+if [[ $HAS_BEEN_INSTALLED_MIUIContentExtension_APK == *"package:com.miui.contentextension"* ]]; then
+  ui_print "- 已经完成传送门的安装，正在修补传送门的相关权限~"
+  pm grant com.miui.contentextension android.permission.WRITE_SECURE_SETTINGS
+  ui_print "- 好诶，《HyperOS For Pad 传送门补丁》安装/更新完成，重启系统后生效！"
+else
+  ui_print "- 坏诶，《HyperOS For Pad 传送门补丁》安装失败，请尝试重新安装！"
+fi
