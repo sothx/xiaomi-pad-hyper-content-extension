b3c99541f25cf347b02e96c72fbd20d692ec476b
sothx <i@sothx.com>
2024-05-29 03:35:18 +0800

    feat: update

--- special/customize.sh
+++ special/customize.sh
@@ -2,6 +2,8 @@
 # shellcheck disable=SC2034
 SKIPUNZIP=0
 . "$MODPATH"/util_functions.sh
+magisk_path=/data/adb/modules/
+module_id=$(grep_prop id $MODPATH/module.prop)
 
 if [[ "$KSU" == "true" ]]; then
   ui_print "- KernelSU 用户空间当前的版本号: $KSU_VER_CODE"
@@ -13,6 +15,12 @@ else
     ui_print "! 请安装 Magisk 26.0+"
     abort "*********************************************"
   fi
+fi
+
+if [[ -d "$magisk_path$module_id" ]];then
+    ui_print "*********************************************"
+    ui_print "模块不允许覆盖安装，请卸载模块并重启平板后再尝试安装！"
+    abort "*********************************************"
 fi
 
 ui_print "- 正在为你修补传送门的权限，请稍等~"
